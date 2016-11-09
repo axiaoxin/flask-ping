f71a00fb02bb8b2a0be65ba23b703edf3a2ba67f
阿小信大人 <254606826@qq.com>
2016-11-09 13:45:37 +0800

    Update extensions.py

--- service/extensions.py
+++ service/extensions.py
@@ -2,12 +2,10 @@
 from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 db = SQLAlchemy(app)
-mail = Mail(app)
 
 app.config.from_pyfile('settings.py')
