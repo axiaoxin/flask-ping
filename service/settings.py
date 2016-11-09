fe871bf171f461f63c0ccef32fc06fb89a0f1313
阿小信大人 <254606826@qq.com>
2016-11-09 13:15:03 +0800

    Update settings.py

--- service/settings.py
+++ service/settings.py
@@ -3,12 +3,12 @@ from decouple import config
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-API_URL = config('API_URL', default='http://localhost')
+API_URL = config('API_URL', default='http://localhost:5000')
 
 DEFAULT_LOG_FILE = config(
-    "DEFAULT_LOG_FILE", default='/var/log/flask-ping/app.log')
+    "DEFAULT_LOG_FILE", default='/tmp/app.log')
 CRONTAB_LOG_FILE = config(
-    "CRONTAB_LOG_FILE", default='/var/log/flask-ping/crontab.log')
+    "CRONTAB_LOG_FILE", default='/tmp/crontab.log')
 
 JSON_AS_ASCII = False
 
