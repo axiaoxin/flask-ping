5479b04c05f214884565070957446f03b625b20a
axiaoxin <254606826@qq.com>
2017-02-28 14:43:06 +0800

    use gunicorn conf file

---
+++ gunicorn_cfg.py
@@ -0,0 +1,14 @@
+﻿from multiprocessing import cpu_count
+
+proc_name = 'flask-ping'
+bind = '127.0.0.1:1111'
+workers = cpu_count() * 2 + 1
+worker_class = 'gevent'
+reload = True
+pidfile = '/var/run/flask-ping.pid'
+raw_env = []
+pythonpath = '/srv/flask-ping/service'
+accesslog = '/data/log/flask-ping/gunicorn.acc.log'
+access_log_format = '%(t)s %(h)s %(s)s "%(r)s" "%(f)s" "%(a)s" "%({Header}i)s"'
+errorlog = '/data/log/flask-ping/gunicorn.err.log'
+
