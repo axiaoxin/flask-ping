e451ed3b1b021473752fcbedf835158cf3d265bf
ashinchen <ashinchen@tencent.com>
2017-12-12 15:38:44 +0800

    fix age type

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ USE db_demo;
 CREATE TABLE `tb_demo` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `name` VARCHAR(64) NULL COMMENT '姓名',
-  `age` VARCHAR(24) NULL COMMENT '年龄',
+  `age` INT NULL COMMENT '年龄',
   `is_deleted` INT NOT NULL DEFAULT 0 COMMENT '是否删除: 0未删除, 1已删除',
   `created_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
   `updated_at` DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '更新时间',
