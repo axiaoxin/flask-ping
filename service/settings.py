132d73d7f24985ff67f3fb0db3fef823fcb116da
ashinchen <ashinchen@tencent.com>
2017-12-12 20:03:55 +0800

    update setting

--- service/settings.py
+++ service/settings.py
@@ -18,8 +18,8 @@ SENTRY_DSN = config('SENTRY_DSN', default=None)
 LOG_PATH = config("LOG_PATH", default='/tmp/')
 if not os.path.exists(LOG_PATH):
     os.makedirs(LOG_PATH)
-LOG_FUNC_CALL = config('LOG_FUNC_CALL', default=True, cast=bool)
-LOG_PEEWEE_SQL = config('LOG_PEEWEE_SQL', default=True, cast=bool)
+LOG_FUNC_CALL = config('LOG_FUNC_CALL', default=False, cast=bool)
+LOG_PEEWEE_SQL = config('LOG_PEEWEE_SQL', default=False, cast=bool)
 
 REDIS_URL = config('REDIS_URL', default='redis://localhost:6379/0')
-CACHE_GET_RESPONSE = config('CACHE_GET_RESPONSE', default=True, cast=bool)
+CACHE_GET_RESPONSE = config('CACHE_GET_RESPONSE', default=False, cast=bool)
