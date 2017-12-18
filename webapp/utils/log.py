69d23ca52385f2a43dd8b900ceeadbdb2919d09f
axiaoxin <254606826@qq.com>
2017-12-18 13:01:37 +0800

    add requests request to curl

--- webapp/utils/log.py
+++ webapp/utils/log.py
@@ -165,3 +165,16 @@ def log_func_call(func):
         return func(*func_args, **func_kwargs)
 
     return wrapper
+
+
+def to_curl(request):
+    headers = ["'{0}: {1}'".format(k, v) for k, v in request.headers.items()]
+    headers = " -H ".join(sorted(headers))
+
+    command = "curl -X {method} -H {headers} -d '{data}' '{uri}'".format(
+        data=request.body or "",
+        headers=headers,
+        method=request.method,
+        uri=request.url,
+    )
+    return command
