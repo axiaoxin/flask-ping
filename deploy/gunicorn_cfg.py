2d5547ac7681d6bb115e0bbcde35619ae2a74c63
axiaoxin <254606826@qq.com>
2017-12-22 16:19:58 +0800

    update gunicorn config

--- deploy/gunicorn_cfg.py
+++ deploy/gunicorn_cfg.py
@@ -1,13 +1,19 @@
 from multiprocessing import cpu_count
+import os
+
+webapp_path = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+    'webapp'
+)
 
 proc_name = 'flask-skeleton'
 bind = '127.0.0.1:5000'
 workers = cpu_count() * 2 + 1
 worker_class = 'gevent'
 reload = True
-pidfile = '/var/run/flask-skeleton.pid'
+pidfile = '/tmp/flask-skeleton.pid'
 raw_env = []
-pythonpath = '/srv/flask-skeleton/service'
-accesslog = '/data/log/flask-skeleton/gunicorn.acc.log'
+pythonpath = webapp_path
+accesslog = '/tmp/gunicorn.acc.log'
 access_log_format = '%(t)s %(h)s %(s)s "%(r)s" %(b)s "%(f)s" "%(a)s" "%({Header}i)s"'
-errorlog = '/data/log/flask-skeleton/gunicorn.err.log'
+errorlog = '/tmp/gunicorn.err.log'
