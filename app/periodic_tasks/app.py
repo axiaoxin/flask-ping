ddc9195d71cf8be4bdda62d22efed910f9bd8172
axiaoxin <254606826@qq.com>
2018-01-29 14:31:00 +0800

    add celery logger

--- app/periodic_tasks/app.py
+++ app/periodic_tasks/app.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from celery import Celery
-
+from celery.utils.log import get_task_logger
 
 app = Celery('tasks')
 app.config_from_object('config')
+logger = get_task_logger(__name__)
 
 
 if __name__ == '__main__':
