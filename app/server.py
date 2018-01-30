d1e49e188a8774a1d3211f56f98e0126903ef6b8
axiaoxin <254606826@qq.com>
2018-01-30 15:38:24 +0800

    fix error handler

--- app/server.py
+++ app/server.py
@@ -5,20 +5,24 @@ from flask import g
 from utils.response import response, ResponseCode
 from blueprints.demo import demo_bp
 from extensions import app, sentry
+import settings
 
 
 @app.errorhandler(404)
 def api_not_found(error):
-    return response(ResponseCode.ENTRY_NOT_FOUND)
+    return response(code=ResponseCode.ENTRY_NOT_FOUND)
 
 
-@app.errorhandler(500)
+@app.errorhandler(Exception)
 def server_error(error):
-    data = {
-        'sentry_event_id': g.sentry_event_id,
-        'public_dsn': sentry.client.get_public_dsn('http')
-    }
-    return response(ResponseCode.SERVER_ERROR, data=data)
+    if settings.SENTRY_DSN:
+        data = {
+            'sentry_event_id': g.sentry_event_id,
+            'public_dsn': sentry.client.get_public_dsn('http')
+        }
+    else:
+        data = None
+    return response(data, ResponseCode.SERVER_ERROR)
 
 
 @app.route('/')
